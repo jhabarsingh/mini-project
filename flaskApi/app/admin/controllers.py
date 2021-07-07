8cf19af73240280c6827c36b51a977e035058633
Aditya Madhyastha <adityasm1238@gmail.com>
2021-07-07 17:58:10 +0530

    Added admin route middleware

--- flaskApi/app/admin/controllers.py
+++ flaskApi/app/admin/controllers.py
@@ -1,9 +1,10 @@
 from flask import Blueprint,request,jsonify
 from flask_jwt_extended import get_jwt_identity,jwt_required
 
-from app.error import handleErrors,AppError
+from app.error import handleErrors
 from app.models import  UserDeploymentRequest,Users
 from app.utils import addUsernames
+from app.middleware import adminRoute
 
 admin = Blueprint('admin',__name__,url_prefix='/admin')
 
@@ -12,8 +13,7 @@ admin = Blueprint('admin',__name__,url_prefix='/admin')
 def getRequets():
     identity = get_jwt_identity()
     user = Users.objects(id = identity).first()
-    print(user.role)
-    if(user.role=="Admin"):
+    if(user.role=="admin"):
         requirements = UserDeploymentRequest.objects
         requirements = list(map(addUsernames,requirements))
         pending = list(filter(lambda x:x['status']=='pending',requirements))
@@ -59,49 +59,44 @@ def getRequets():
 
 @admin.route('/get-users',methods=['POST'])
 @jwt_required()
+@adminRoute
 def getUsers():
-    identity = get_jwt_identity()
-    user = Users.objects(id = identity).first()
-    if(user.role=="Admin"):
-        users = Users.objects.exclude('id').exclude('password')
-        admins = list(filter(lambda x:x.role=='admin',users))
-        userss = list(filter(lambda x:x.role=='user',users))
-        response = jsonify({'data':{
-            'admins':{
-                'count':len(admins),
-                'array':admins
-            },
-            'users':{
-                'count':len(userss),
-                'array':userss
-            }
-        }})
-        return response,200
-    return AppError.badRequest("Admin only route")
+    users = Users.objects.exclude('id').exclude('password')
+    admins = list(filter(lambda x:x.role=='admin',users))
+    userss = list(filter(lambda x:x.role=='user',users))
+    response = jsonify({'data':{
+        'admins':{
+            'count':len(admins),
+            'array':admins
+        },
+        'users':{
+            'count':len(userss),
+            'array':userss
+        }
+    }})
+    return response,200
 
 
 @admin.route('/update-requirement',methods=['POST'])
 @jwt_required()
 @handleErrors
+@adminRoute
 def updateRequirement():
-    identity = get_jwt_identity()
-    user = Users.objects(id = identity).first()
-    if(user.role=="Admin"):
-        data = request.get_json()
-        _id =  data['id']
-        req = UserDeploymentRequest.objects(id=_id).first()
-        if 'cpu' in data:
-            req.cpu = data['cpu']
-        if 'memory' in data:
-            req.memory = data['memory']
-        if 'cpuLimit' in data:
-            req.cpuLimit = data['cpuLimit']
-        if 'memLimit' in data:
-            req.memLimit = data['memLimit']
-        if 'maxReplicas' in data:
-            req.maxReplicas = data['maxReplicas']
-        if 'maxRuntime' in data:
-            req.maxRuntime = data['maxRuntime']
-        req.save()
-        return {'message':'Updated Successfully'},200
-    return AppError.badRequest("Admin only route")
+    data = request.get_json()
+    _id =  data['id']
+    req = UserDeploymentRequest.objects(id=_id).first()
+    if 'cpu' in data:
+        req.cpu = data['cpu']
+    if 'memory' in data:
+        req.memory = data['memory']
+    if 'cpuLimit' in data:
+        req.cpuLimit = data['cpuLimit']
+    if 'memLimit' in data:
+        req.memLimit = data['memLimit']
+    if 'maxReplicas' in data:
+        req.maxReplicas = data['maxReplicas']
+    if 'maxRuntime' in data:
+        req.maxRuntime = data['maxRuntime']
+    req.save()
+    return {'message':'Updated Successfully'},200
+    
